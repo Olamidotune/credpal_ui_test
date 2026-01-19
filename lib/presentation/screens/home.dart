638262a10a029a4717cffdc7fe10687e9eb2e88f
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-19 23:51:14 +0100

    feat: add modal bottom sheet to display merchants on 'View all' tap

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -109,7 +109,89 @@ class HomeScreen extends StatelessWidget {
                       style: TextStyles.bodyTextBold.copyWith(fontSize: 16),
                     ),
                     GestureDetector(
-                      onTap: () {},
+                      onTap: () {
+                        showModalBottomSheet(
+                          context: context,
+                          isScrollControlled: true,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(24),
+                            ),
+                          ),
+                          builder: (context) {
+                            return SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.5,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: AppSpacing.horizontalSpacing,
+                                ),
+                                child: Column(
+                                  children: [
+                                    AppSpacing.verticalSpaceMedium,
+                                    Center(
+                                      child: Container(
+                                        margin: const EdgeInsets.only(
+                                          bottom: 12,
+                                        ),
+                                        height: 4,
+                                        width: 40,
+                                        decoration: BoxDecoration(
+                                          color: AppColors.blackColor,
+                                          borderRadius: BorderRadius.circular(
+                                            20,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    AppSpacing.verticalSpaceLarge,
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: merchants
+                                          .take(4)
+                                          .map(
+                                            (merchant) => MerchantCard(
+                                              merchant: merchant,
+                                            ),
+                                          )
+                                          .toList(),
+                                    ),
+                                    AppSpacing.verticalSpaceMedium,
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: merchants
+                                          .skip(4)
+                                          .take(4)
+                                          .map(
+                                            (merchant) => MerchantCard(
+                                              merchant: merchant,
+                                            ),
+                                          )
+                                          .toList(),
+                                    ),
+                                    AppSpacing.verticalSpaceMedium,
+                                    Row(
+                                      children: List.generate(4, (index) {
+                                        if (index < 2) {
+                                          return Expanded(
+                                            child: MerchantCard(
+                                              merchant: merchants[8 + index],
+                                            ),
+                                          );
+                                        }
+                                        return const Expanded(
+                                          child: SizedBox(),
+                                        );
+                                      }),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
                       child: Text(
                         'View all',
                         style: TextStyles.bodyText.copyWith(

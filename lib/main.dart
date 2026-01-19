77361ffe1b27195c9235c252d1d836809ebcbc05
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-20 00:10:54 +0100

    feat: enable portrait orientation support in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:credpal_ui_test/presentation/screens/home.dart';
 import 'package:credpal_ui_test/router/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
 
 class MyApp extends StatelessWidget {

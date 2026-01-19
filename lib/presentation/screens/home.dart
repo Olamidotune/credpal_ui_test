07cd83455fa5ac741f12bea5dada9e1b9b559747
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-19 20:03:43 +0100

    feat: enhance HomeScreen with search and scan functionality, add SVG assets

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:credpal_ui_test/core/constants/app_spacing.dart';
 import 'package:credpal_ui_test/core/theme/text_styles.dart';
 import 'package:credpal_ui_test/presentation/widgets/button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -32,6 +33,72 @@ class HomeScreen extends StatelessWidget {
         ),
 
         toolbarHeight: 140,
+      ),
+
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppSpacing.horizontalSpacing,
+            ),
+            child: Column(
+              children: [
+                AppSpacing.verticalSpaceMedium,
+                Row(
+                  children: [
+                    CustomSearchBar(),
+                    AppSpacing.horizontalSpaceMedium,
+                    ScanButton(),
+                  ],
+                ),
+                Text('data'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ScanButton extends StatelessWidget {
+  const ScanButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: AppColors.secondaryColor.withValues(alpha: .3),
+        borderRadius: BorderRadius.circular(12),
+      ),
+
+      child: SvgPicture.asset('assets/svg/scan.svg'),
+    );
+  }
+}
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SearchBar(
+        leading: SvgPicture.asset(
+          'assets/svg/search.svg',
+          width: 18,
+          height: 18,
+        ),
+        hintText: 'Search for products or stores',
+        hintStyle: WidgetStateProperty.all(
+          TextStyles.bodyText.copyWith(color: AppColors.greyColor),
+        ),
+        backgroundColor: WidgetStateProperty.all(
+          AppColors.secondaryColor.withValues(alpha: .3),
+        ),
+        elevation: WidgetStateProperty.all(0),
       ),
     );
   }

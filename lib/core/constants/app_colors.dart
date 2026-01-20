106edc60a9d4f80e763913bad586237a8fa7b06e
Egundeyi Oladotun <Davidegundeyi@gmail.com>
2026-01-20 10:38:35 +0100

    refactor: remove unused color constants and theme data for cleaner codebase

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,18 +3,9 @@ import 'dart:ui';
 class AppColors {
   static const primaryColor = Color(0xFF274FED);
   static const secondaryColor = Color(0xFFD0DAFF);
-  static const greenColor = Color(0xFF34C759);
   static const whiteColor = Color(0xFFFFFFFF);
-  static const redColor = Color(0xFFFF3B30);
-  static const primaryTextColor = Color(0xFF373737);
-  static const secondaryTexColor = Color(0xFF171717);
   static const greyColor = Color(0xFF8E8E93);
   static const blackColor = Color(0xFF000000);
-  static final shadowColor = const Color(0xFF000000).withValues(alpha: 0.1);
-  static const textColor = Color(0xFF1F1F1F);
-  static const darkBg = Color(0xFF0A0F1A);
-  static const darkSurface = Color(0xFF121826);
-  static const lightBg = Color(0xFFF9FAFB);
   static const yellowColor = Color(0xFFFEFE00);
 
   static const justRiteColor = Color(0xFF000065);
